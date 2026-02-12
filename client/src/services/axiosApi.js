fd6aa22406c7586c2f1174652ac0d4da22846a9b
OlamideZiyad <olamidezyad@gmail.com>
2026-02-12 22:35:27 +0100

    Update : VITE API URL

--- client/src/services/axiosApi.js
+++ client/src/services/axiosApi.js
@@ -10,7 +10,7 @@ const apiClient = axios.create({
 
 /* ===============================
    INTERCEPTOR AUTH
-================================ */
+================================ *///
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token"); // localStorage recommandé
