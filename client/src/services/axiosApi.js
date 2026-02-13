06b3d262844401a7dfc99593d9b9ab4b72d566b4
OlamideZiyad <olamidezyad@gmail.com>
2026-02-13 11:35:18 +0100

    Update database.js for deployement and local

--- client/src/services/axiosApi.js
+++ client/src/services/axiosApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-/* ===============================
+/* ===============================/
    INTERCEPTOR AUTH
 ================================ *///
 apiClient.interceptors.request.use(
--- server/configurations/database.js
+++ server/configurations/database.js
@@ -1,18 +1,20 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-// Charger les variables d'environnement
-require('dotenv').config();
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "mysql",
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        logging: false,
+      }
+    );
 
-// Configuration de la connexion à la base de données MySQL
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,    
-    dialect: 'mysql',            
-    port: process.env.DB_PORT,
-  }
-);
-// Exporter l'instance de Sequelize
 module.exports = sequelize;
